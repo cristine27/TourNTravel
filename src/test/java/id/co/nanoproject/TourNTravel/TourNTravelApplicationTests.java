deb2415479fe06164c7004841a9534f994b2e020
richardhanslie <richardhanslie@gmail.com>
2023-04-11 15:55:37 +0700

    first commit

---
+++ src/test/java/id/co/nanoproject/TourNTravel/TourNTravelApplicationTests.java
@@ -0,0 +1,13 @@
+package id.co.nanoproject.TourNTravel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourNTravelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
