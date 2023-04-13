7ce389f544496ae8d31a8f8568519878e9c37ca9
Cristine <31438967+cristine27@users.noreply.github.com>
2023-04-13 14:18:26 +0700

    fix orderTransaction

--- src/main/java/id/co/nanoproject/TourNTravel/service/OrderService.java
+++ src/main/java/id/co/nanoproject/TourNTravel/service/OrderService.java
@@ -2,13 +2,14 @@ package id.co.nanoproject.TourNTravel.service;
 
 import id.co.nanoproject.TourNTravel.entity.OrderReq;
 import id.co.nanoproject.TourNTravel.entity.OrderResp;
+import id.co.nanoproject.TourNTravel.model.OrderTransaction;
 import id.co.nanoproject.TourNTravel.repo.OrderRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrderService {
-
+    private final String MENUNGGU_PEMBAYARAN = "MENUNGGU_PEMBAYARAN";
     @Autowired
     public OrderRepo repo;
 
@@ -22,10 +23,16 @@ public class OrderService {
         String custName = custService.findNamaById(orderReq.getId_cust());
         String hargaPaket = paketService.findHargaById(orderReq.getId_paket());
 
+        OrderTransaction newOrder = new OrderTransaction();
+        newOrder.setId_paket(orderReq.getId_paket());
+        newOrder.setId_cust(orderReq.getId_cust());
+        newOrder.setStatus_pembayaran(MENUNGGU_PEMBAYARAN);
+        repo.save(newOrder);
+
         OrderResp resp = new OrderResp();
         resp.setNamaCust(custName);
         resp.setHarga(hargaPaket);
-        resp.setStatus_pembayaran("MENUNGGU PEMBAYARAN");
+        resp.setStatus_pembayaran(MENUNGGU_PEMBAYARAN);
         return resp;
     }
 }
